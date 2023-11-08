5de0fe125f4f7041b34ee64fc43330e947921e36
Jerome226 <55724525+Jerome226@users.noreply.github.com>
2023-11-07 20:25:32 -0500

    Update AllConfig.java

--- src/main/java/mod/ckenja/terrafirmaboiler/AllConfig.java
+++ src/main/java/mod/ckenja/terrafirmaboiler/AllConfig.java
@@ -9,7 +9,7 @@ public class AllConfig {
     static {
         ForgeConfigSpec.Builder configBuilder = new ForgeConfigSpec.Builder();
         activeBoilerHeaterLevelConfigEntry = configBuilder.defineInRange("active_boiler_heater_level", 1.0, -1.0, Double.MAX_VALUE);
-        charcoalForgeHeaterLevelScaleConfigEntry = configBuilder.defineInRange("charcoal_forge_heater_level_scale", 2.0, -1.0, Double.MAX_VALUE);
+        charcoalForgeHeaterLevelScaleConfigEntry = configBuilder.defineInRange("charcoal_forge_heater_level_scale", 1.0, -1.0, Double.MAX_VALUE);
         SERVER_CONFIG = configBuilder.build();
     }
 }
